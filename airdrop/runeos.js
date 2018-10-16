e52ce4c5efeb1131e28aeed68faf45a782360519
balex8888 <balex8888@gmail.com>
2018-10-16 02:07:13 -0700

    Jungle and Mainnet connection established, playing with specific api calls

--- airdrop/runeos.js
+++ airdrop/runeos.js
@@ -1,24 +1,53 @@
 Eos = require('eosjs')
  
-// Private key or keys (array) provided statically or by way of a function.
-// For multiple keys, the get_required_keys API is used (more on that below).
-keyProvider: '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+const chain = {
+  'main': 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906', // main network
+  'jungle': '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca', // jungle testnet
+  'sys': 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f' // local developer
+};
+// const httpEndpoint =  'https://eos.greymass.com/' // Mainnet Http Endpoint
+const httpEndpoint =  'http://193.93.219.219:8888/' // Jungle Http Endpoint
+const chainId = chain.jungle;
+const keyProvider = '5KMg2WbZXpkrXcFTpxt6SbwYw9RQGawGsibfYYLy6ydMZZqKFkq'
+
+
+// Default configuration
+// config = {
+//   chainId: null, // 32 byte (64 char) hex string
+//   keyProvider: ['PrivateKeys...'], // WIF string or array of keys..
+//   httpEndpoint: 'http://127.0.0.1:8888',
+//   expireInSeconds: 60,
+//   broadcast: true,
+//   verbose: false, // API activity
+//   sign: true
+// }
  
-// Localhost Testnet (run ./docker/up.sh)
-eos = Eos({keyProvider})
- 
+const mainnetConfig = {
+  keyProvider: '5KMg2WbZXpkrXcFTpxt6SbwYw9RQGawGsibfYYLy6ydMZZqKFkq', // Dummy Jungle Key
+  httpEndpoint: 'http://eos.greymass.com/',
+  // httpEndpoint: 'http://ayeaye.cypherglass.com:8888/v1/chain/get_info',
+  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+};
+const jungleConfig = {
+  keyProvider: '5KMg2WbZXpkrXcFTpxt6SbwYw9RQGawGsibfYYLy6ydMZZqKFkq', // Dummy Jungle Key
+  httpEndpoint: 'http://193.93.219.219:8888/',
+  chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',
+};
+
+
 // Connect to a testnet or mainnet
-eos = Eos({httpEndpoint, chainId, keyProvider})
+// eos = Eos({httpEndpoint, chainId, keyProvider})
+eos = Eos(jungleConfig);
+// eos = Eos(mainnetConfig);
+
  
-// Cold-storage
-eos = Eos({httpEndpoint: null, chainId, keyProvider})
- 
-// Add support for non-EOS public key prefixes, such as PUB, etc
-eos = Eos({keyPrefix: 'PUB'})
- 
-// Read-only instance when 'eosjs' is already a dependency
-eos = Eos.modules.api({/*config*/})
- 
-// Read-only instance when an application never needs to write (smaller library)
-EosApi = require('eosjs-api')
-eos = EosApi({/*config*/})
+// console.log(eos)
+// console.log(eos.getInfo((error, result) => { console.log(error, result) }))
+// console.log(eos.getInfo({}))
+const run = async function () {
+  // var info = await eos.getInfo({}) 
+
+}
+run()
+
+
