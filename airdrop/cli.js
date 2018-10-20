a64bf8af85ea0fe6c3b1b272b0bace844f3896f2
balex8888 <balex8888@gmail.com>
2018-10-19 19:14:01 -0700

    Updating Echo callouts

--- airdrop/cli.js
+++ airdrop/cli.js
@@ -196,6 +196,7 @@ const airdropGenerator = (formattedSnapshotData, accountName, tokenName, airdrop
   CREATED=$(cleos -u http://193.93.219.219:8888/ get table $ISSUER_ACCOUNT $TOKEN_SYMBOL stat | grep $TOKEN_SYMBOL)
   if [[ -z $CREATED ]]; then
       echo "Creating token: \"$TOKEN_SYMBOL\", with a max supply of: \"$MAX_TOKEN_SUPPLY\", under account: \"$ISSUER_ACCOUNT\"..."
+      echo cleos -u http://193.93.219.219:8888/ push action $ISSUER_ACCOUNT create "[\"$ISSUER_ACCOUNT\", \"$MAX_TOKEN_SUPPLY $TOKEN_SYMBOL\"]" -p $ISSUER_ACCOUNT@active
       cleos -u http://193.93.219.219:8888/ push action $ISSUER_ACCOUNT create "[\"$ISSUER_ACCOUNT\", \"$MAX_TOKEN_SUPPLY $TOKEN_SYMBOL\"]" -p $ISSUER_ACCOUNT@active
   else
       echo "Token \"$TOKEN_SYMBOL\" already exist -- Skipping Create."
@@ -204,6 +205,7 @@ const airdropGenerator = (formattedSnapshotData, accountName, tokenName, airdrop
   ISSUANCE=$(cleos -u http://193.93.219.219:8888/ get table $ISSUER_ACCOUNT $ISSUER_ACCOUNT accounts | grep $TOKEN_SYMBOL)
   if [[ -z $ISSUANCE ]]; then
       echo "Issuing initial supply of: \"$INITIAL_TOKEN_SUPPLY $TOKEN_SYMBOL\" to account \"$ISSUER_ACCOUNT\"..."
+      echo cleos -u http://193.93.219.219:8888/ push action $ISSUER_ACCOUNT issue "[\"$ISSUER_ACCOUNT\", \"$INITIAL_TOKEN_SUPPLY $TOKEN_SYMBOL\", \"initial supply\"]" -p $ISSUER_ACCOUNT@active
       cleos -u http://193.93.219.219:8888/ push action $ISSUER_ACCOUNT issue "[\"$ISSUER_ACCOUNT\", \"$INITIAL_TOKEN_SUPPLY $TOKEN_SYMBOL\", \"initial supply\"]" -p $ISSUER_ACCOUNT@active
   else
       echo "Token already issued to \"$ISSUER_ACCOUNT\" -- Skipping issue"
@@ -215,6 +217,7 @@ const airdropGenerator = (formattedSnapshotData, accountName, tokenName, airdrop
       CURRENT_BALANCE=$(cleos -u http://193.93.219.219:8888/ get table $ISSUER_ACCOUNT $ACCOUNT accounts | grep $TOKEN_SYMBOL) 
       if [[ -z $CURRENT_BALANCE ]]; then
           echo "Airdropping $AMOUNT $TOKEN_SYMBOL to $ACCOUNT"
+          echo cleos -u http://193.93.219.219:8888/ push action $ISSUER_ACCOUNT transfer "[\"$ISSUER_ACCOUNT\", \"$ACCOUNT\", \"$AMOUNT $TOKEN_SYMBOL\", \"airdrop\"]" -p $ISSUER_ACCOUNT@active
           cleos -u http://193.93.219.219:8888/ push action $ISSUER_ACCOUNT transfer "[\"$ISSUER_ACCOUNT\", \"$ACCOUNT\", \"$AMOUNT $TOKEN_SYMBOL\", \"airdrop\"]" -p $ISSUER_ACCOUNT@active
       else
           echo "Skipping $ACCOUNT"
@@ -258,7 +261,7 @@ const runShell = async () => {
   // shell.exec('cleos -u http://193.93.219.219:8888/ get table junglefoxfox junglefoxfox accounts')
 }
 
-const runAirdrop = async () => {
+const runAirdrop = async () => {s
   init();
   
   /*    Sample Answers (for quick testing) */
