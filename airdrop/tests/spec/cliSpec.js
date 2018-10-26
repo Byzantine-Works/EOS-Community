a036e86c2705c56e1125638901420924356b3801
balex8888 <balex8888@gmail.com>
2018-10-25 19:23:18 -0700

    Figlet ASCII Art Added - Very Important.

--- airdrop/tests/spec/cliSpec.js
+++ airdrop/tests/spec/cliSpec.js
@@ -1,11 +1,20 @@
 'use strict';
-console.log("~~~~~ In CLI Spec ~~~~~~~")
 
 const cli = require('../../cli.js');
 const assert = require("assert");
 const chai = require("chai");
 const should = chai.should();
-
+const figlet = require('figlet');
+const chalk = require('chalk');
+console.log(
+  chalk.green(
+      figlet.textSync("Airdrop Tool Unit Tests", {
+          font: "Standard",
+          horizontalLayout: "default",
+          verticalLayout: "default"
+      })
+  )
+);
 
 
 describe('RAM Price Calculator', function() {
@@ -13,16 +22,16 @@ describe('RAM Price Calculator', function() {
     should.exist(cli.getPriceEstimate);
   });
   it('should be a function', function() {
-    cli.getPriceEstimate.should.be.a.Function;
+    cli.getPriceEstimate.should.be.a.function;
   });
 });
 
-  describe('runAirdrop', function() {
+  describe('run', function() {
     it('should exist', function() {
-      should.exist(cli.runAirdrop);
+      should.exist(cli.run);
     });
     it('should be a function', function() {
-      cli.runAirdrop.should.be.a.Function;
+      (typeof(cli.run)).should.equal('function')
     });
   });
 
