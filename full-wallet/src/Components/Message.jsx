0576ca04927821cf2dadb28767f7439a5c096036
VernSul <vicfaucon@gmail.com>
2018-11-04 23:49:42 -0800

    -Fix after merge

--- full-wallet/src/Components/Message.jsx
+++ full-wallet/src/Components/Message.jsx
@@ -8,7 +8,7 @@ const Message = props => {
 
     return (
         <div className="MessageBox">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSercRxm2e9fGGti06PYgUFj42sN4LFOuG801rldzZ-sah7LmQ/viewform?embedded=true" width="640" height="945" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSercRxm2e9fGGti06PYgUFj42sN4LFOuG801rldzZ-sah7LmQ/viewform?embedded=true" width="640" height="945" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
         </div>
     )
 
