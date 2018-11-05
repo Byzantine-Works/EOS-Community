e73c53da8c9297cc92be6b99b056fe73bcd119f0
balex8888 <balex8888@gmail.com>
2018-11-05 13:42:56 -0800

    Base router setup

--- airdrop/app/server/index.js
+++ airdrop/app/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
+const router = require('./router.js');
+
 
 const app = express();
 const port = 9001;
@@ -11,6 +13,7 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../public')));
 
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Airdrop Toolkit is running on Port: ${port}`);
