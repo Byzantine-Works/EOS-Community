541f6c7fff97e2036d4ad68a5f80d7ff32cf5283
balex8888 <balex8888@gmail.com>
2018-11-06 00:30:51 -0800

    Client + Server fully merged back into app folder

--- airdrop/app/src/components/home.js
+++ airdrop/app/src/components/home.js
@@ -1,6 +1,60 @@
 import React, {Component} from 'react';
 import Header from './header';
 import Footer from './footer';
+import $ from "jquery";
+import axios from 'axios';
+
+function handleClick(e) {
+    e.preventDefault();
+    $('.formInner').css('display', 'block');
+    $('.detailInner').css('display', 'block');
+    console.log('submit button clicked')
+
+    var dummydata = {
+        'ACCOUNT_NAME': '',
+        'TOKEN_NAME': '',
+        'MAX_TOKEN_SUPPLY': '',
+        'SNAPSHOT_MONTH': '',
+        'MIN_EOS_HELD': '',
+        'MAX_EOS_HELD': '',
+        'RATIO_OR_FLAT': '',
+        'AIRDROP_RATIO': '',
+        'AIRDROP_FLAT': '',
+    };
+
+    // axios.get('http://localhost:9001/get_price')
+    // .then((res) => {
+    //     console.log('Client GET Res :', res);
+    //     })
+    // .catch((err) => {
+    //    console.log('axios GET err: ', err);
+    //   })
+
+    axios.post('http://localhost:9001/get_estimate', dummydata)
+    .then((res) => {
+        console.log('Client POST Res :', res);
+        })
+    .catch((err) => {
+       console.log('axios POST err: ', err);
+      })
+    
+  }
+
+function downloadButton(e) {
+    console.log('downloadButton clicked')
+
+    var userParams = {
+        'ACCOUNT_NAME': '',
+        'TOKEN_NAME': '',
+        'MAX_TOKEN_SUPPLY': '',
+        'SNAPSHOT_MONTH': '',
+        'MIN_EOS_HELD': '',
+        'MAX_EOS_HELD': '',
+        'RATIO_OR_FLAT': '',
+        'AIRDROP_RATIO': '',
+        'AIRDROP_FLAT': '',
+    }      
+}
 
 class Home extends Component{
     render(){
@@ -14,18 +68,18 @@ class Home extends Component{
                     <div className="container">
                         <div className="formInner">
                             <div className="formWrap">
-                                <form action="/detail">
+                                <form>
                                     <label>Account Name</label>
                                     <input type="text" maxlength='12' />
                                     <label>Token Name</label>
                                     <input type="text" maxlength='7' />
                                     <label>Max token supply</label>
-                                    <input type="text" />
+                                    <input type="text" placeholder="1000000" />
                                     <label>Snapshot month</label>
                                     <select>
-                                        <option value="">Select</option>
+                                        {/* <option value="">Select</option> */}
                                         <option value="">Genesis</option>
-                                        <option value="">jungle Testent</option>
+                                        <option value="">Jungle Testent</option>
                                         <option value="">July</option>
                                         <option value="">August</option>
                                         <option value="">September</option>
@@ -45,6 +99,7 @@ class Home extends Component{
                                     </select>
                                     <label>Max EOS held</label>
                                     <select>
+                                        <option value="">No Max</option>
                                         <option value="">0</option>
                                         <option value="">1</option>
                                         <option value="">10</option>
@@ -62,11 +117,27 @@ class Home extends Component{
                                     <label>Airdrop ratio</label>
                                     <input type="number" />
                                     <label>Airdrop flat</label>
-                                    <input type="number" />
-                                    <input type="submit" />
+                                    <input type="number" className="fields" />
+                                    <input type="submit" className="submitButton actives" onClick={handleClick} />
                                 </form>
                             </div>
                         </div>
+                        <div className="formInner detailInner">
+                            <h4>Price Calculation</h4>
+                            <ul>
+                                <li>Number of account <span>160037</span></li>
+                                <li>Ram Required (kb) <span>22725.253</span></li>
+                                <li>CPU-Stake Rough Estimate <span>5.14 EOS</span></li>
+                                <li>Price Estimate EOS <span>2195.6348</span></li>
+                                <li> Price Estimate USD <span>1337.17</span></li>
+                            </ul>
+                            <p>The estimated cost of the airdrop with these settings will be $ 1337.17 USD</p>
+
+                            <button onClick={downloadButton}>
+                                Download
+                            </button>
+                        </div>
+
                     </div>
                 </div>
                 <Footer />
