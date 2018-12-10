9a6648935e67847496cf2465c635c3be87d223f7
VernSul <vicfaucon@gmail.com>
2018-12-09 19:55:02 -0800

    gg

---
+++ iDeos/public/eosio/getWalletStatus.sh
@@ -0,0 +1,12 @@
+# get wallet status (locked or unlocked);
+# returns:
+#
+# Wallets:
+# [
+#   "default *",
+#   "<otherWalletName>"
+# ]
+# 
+# unlocked wallets are marked with an asterix following their name
+#
+/usr/local/bin/cleos wallet list
