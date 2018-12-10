b42de47f06a14fd33ef77e92d0ee94a9b074a05f
VernSul <vicfaucon@gmail.com>
2018-12-09 23:56:45 -0800

    gg

--- iDeos/public/eosio/killTestNet.sh
+++
@@ -1,5 +0,0 @@
-# for demo purposes mostly
-# find and kill nodeos process
-# modified version of https://stackoverflow.com/questions/3510673/find-and-kill-a-process-in-one-line-using-bash-and-regex
-#
-kill $(ps -e | grep nodeos | awk '{print $1}')
